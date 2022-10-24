e23af3a11f187b196914bf40443986d4ba56d023
rockstar <leragnezdina@mail.ru>
2022-10-24 19:58:32 +0900

    asd

--- index.php
+++ index.php
@@ -31,43 +31,19 @@
     // exit;
 	// // Close connection
 	// $smpp->close();
-	$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-	socket_connect($socket, '94.130.198.32', 65432);
-	print_r('123');
-	exit;
-	while (TRUE) {
-			$r = array($socket);
-			$c = socket_select($r, $w = NULL, $e = NULL, 5);
-			
-			foreach ($r as $read_socket) {
-					if ($r = negotiate($read_socket)) {
-							var_dump($r);
-							exit;
-					}
-			}
-	}
-
-	function negotiate ($socket) {
-			socket_recv($socket, $buffer, 1024, 0);
-
-			for ($chr = 0; $chr < strlen($buffer); $chr++) {
-					if ($buffer[$chr] == chr(255)) {
-
-							$send = (isset($send) ? $send . $buffer[$chr] : $buffer[$chr]);
-
-							$chr++;
-							if (in_array($buffer[$chr], array(chr(251), chr(252)))) $send .= chr(254);
-							if (in_array($buffer[$chr], array(chr(253), chr(254)))) $send .= chr(252);
-
-							$chr++;
-							$send .= $buffer[$chr];
-					} else {
-							break;
-					}
-			}
-
-			if (isset($send)) socket_send($socket, $send, strlen($send), 0);
-			if ($chr - 1 < strlen($buffer)) return substr($buffer, $chr);
-
-	}
+	$host    = "94.130.198.32";
+	$port    = 65432;
+	$message = "Hello Server";
+	echo "Message To server :".$message;
+	// create socket
+	$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+	// connect to server
+	$result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
+	// send string to server
+	//socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
+	// get server response
+	$result = socket_read ($socket, 1024) or die("Could not read server response\n");
+	echo "Reply From Server  :".$result;
+	// close socket
+	socket_close($socket);
 ?>
