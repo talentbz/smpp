5c098f0becc9ad002ab39a0758aed01a6e864c2c
rockstar <leragnezdina@mail.ru>
2022-10-24 18:33:13 +0900

    sad

--- transmitter.php
+++ transmitter.php
@@ -1,12 +1,12 @@
 <?php
-//print "<pre>";
+print "<pre>";
 require_once "smpp.php";
 $tx=new SMPP('94.130.198.32',65432);
 
-//$tx->debug=true;
+$tx->debug=true;
 $tx->system_type="WWW";
 $tx->addr_npi=1;
-//print "open status: ".$tx->state."\n";
+print "open status: ".$tx->state."\n";
 $tx->bindTransmitter("papax","cayou3");
 $tx->sms_source_addr_npi=1;
 //$tx->sms_source_addr_ton=1;
@@ -17,5 +17,5 @@ $tx->sendSMS("2121","34632942304","Hello world!");
 // exit;
 $tx->close();
 unset($tx);
-//print "</pre>";
+print "</pre>";
 ?>
