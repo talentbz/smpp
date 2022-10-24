658e3691bdf0f204d57f8d13273539f0baed5efc
rockstar <leragnezdina@mail.ru>
2022-10-24 18:27:27 +0900

    asd

--- sockettransport.class.php
+++ sockettransport.class.php
@@ -228,12 +228,13 @@ class SocketTransport
 		}
 		$it = new ArrayIterator($this->hosts);
 		while ($it->valid()) {
-			list($hostname,$port,$ip6s,$ip4s) = $it->current();
+			
+			// list($hostname,$port,$ip6s,$ip4s) = $it->current();
 
-			print_r('self::$forceIpv4'.self::$forceIpv4);echo('</br>');
-			print_r('$ip6s'.$ip6s);echo('</br>');
-			print_r('self::$forceIpv6'.self::$forceIpv6);echo('</br>');
-			print_R('$ip4s'.$ip4s);echo('</br>');
+			// print_r('self::$forceIpv4'.self::$forceIpv4);echo('</br>');
+			// print_r('$ip6s'.$ip6s);echo('</br>');
+			// print_r('self::$forceIpv6'.self::$forceIpv6);echo('</br>');
+			// print_R('$ip4s'.$ip4s);echo('</br>');
 			if (!self::$forceIpv4 && !empty($ip6s)) { // Attempt IPv6s first
 				echo '12311';
 				foreach ($ip6s as $ip) {
---
+++ transmitter.php
@@ -0,0 +1,18 @@
+<?php
+//print "<pre>";
+require_once "smpp.php";
+$tx=new SMPP('94.130.198.32',65432);
+//$tx->debug=true;
+$tx->system_type="WWW";
+$tx->addr_npi=1;
+//print "open status: ".$tx->state."\n";
+$tx->bindTransmitter("papax","cayou3");
+$tx->sms_source_addr_npi=1;
+//$tx->sms_source_addr_ton=1;
+$tx->sms_dest_addr_ton=1;
+$tx->sms_dest_addr_npi=1;
+$tx->sendSMS("2121","34632942304","Hello world!");
+$tx->close();
+unset($tx);
+//print "</pre>";
+?>
