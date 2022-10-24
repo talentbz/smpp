83d1fcb9a308865da09b176b81910828aa9fb29e
rockstar <leragnezdina@mail.ru>
2022-10-24 19:00:18 +0900

    sdfs

--- sockettransport.class.php
+++ sockettransport.class.php
@@ -216,12 +216,14 @@ class SocketTransport
 	{
 		if (!self::$forceIpv4) {
 			$socket6 = @socket_create(AF_INET6,SOCK_STREAM,SOL_TCP);
+			print_r('$socket6'.$socket6);
 			if ($socket6 == false) throw new SocketTransportException('Could not create socket; '.socket_strerror(socket_last_error()), socket_last_error());
 			socket_set_option($socket6,SOL_SOCKET,SO_SNDTIMEO,$this->millisecToSolArray(self::$defaultSendTimeout));
 			socket_set_option($socket6,SOL_SOCKET,SO_RCVTIMEO,$this->millisecToSolArray(self::$defaultRecvTimeout));
 		}
 		if (!self::$forceIpv6) {
 			$socket4 = @socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
+			print_r('$socket4'.$socket4);
 			if ($socket4 == false) throw new SocketTransportException('Could not create socket; '.socket_strerror(socket_last_error()), socket_last_error());
 			socket_set_option($socket4,SOL_SOCKET,SO_SNDTIMEO,$this->millisecToSolArray(self::$defaultSendTimeout));
 			socket_set_option($socket4,SOL_SOCKET,SO_RCVTIMEO,$this->millisecToSolArray(self::$defaultRecvTimeout));
@@ -229,8 +231,7 @@ class SocketTransport
 		$it = new ArrayIterator($this->hosts);
 		while ($it->valid()) {
 			list($hostname,$port,$ip6s,$ip4s) = $it->current();
-			print_r('$socket6'.$socket6);
-			print_r('$socket4'.$socket4);
+			
 			if (!self::$forceIpv4 && !empty($ip6s)) { // Attempt IPv6s first
 				foreach ($ip6s as $ip) {
 					if ($this->debug) call_user_func($this->debugHandler, "Connecting to $ip:$port...");
